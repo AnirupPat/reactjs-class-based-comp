e2bc27ce52926a82abc06eee4a04316879d2245f
Anirup Patnaik <anirup049@gmail.com>
2021-06-02 02:46:29 +0530

    React Context in Class based component

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,26 +1,29 @@
 import classes from "./UserFinder.module.css";
-import { Fragment, useState, useEffect, Component } from "react";
+import { Fragment, Component } from "react";
+import UsersContext from '../store/users-context';
 
 import Users from "./Users";
 
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
-
 class UserFinder extends Component {
+    static contextType = UsersContext;
   constructor() {
     super();
     this.state = {
-      filteredUsers: DUMMY_USERS,
-      searchTerm: "",
+      filteredUsers: [],
+      searchTerm: '',
     };
   }
+
+  componentDidMount() {
+      this.setState({
+          filteredUsers: this.context.users
+      })
+  }
+
   // this is used in place of useEffect()
   componentDidUpdate(prevProps, prevState) {
       if(prevState.searchTerm !== this.state.searchTerm) {
-        this.setState({filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm))})
+        this.setState({filteredUsers: this.context.users.filter((user) => user.name.includes(this.state.searchTerm))})
       }
   }
   searchChangeHandler(event) {
